02061570083633f979a2d7d9ef21de240792a887
Chia-liang Kao <clkao@clkao.org>
2013-07-29 15:37:58 +0800

    featured projects

--- app/app.ls
+++ app/app.ls
@@ -8,3 +8,9 @@ angular.module "g0v.tw" <[firebase]>
 
 .controller BlogCtrl: <[$scope angularFireCollection fireRoot]> ++ ($scope, angularFireCollection, fireRoot) ->
   $scope.articles = angularFireCollection fireRoot.child("feed/blog/articles").limit 10
+
+.controller FeaturedCtrl: <[$scope angularFireCollection]> ++ ($scope, angularFireCollection) ->
+  g0vhub = new Firebase("https://g0vhub.firebaseio.com/projects")
+  $scope.projects = angularFireCollection g0vhub
+  $scope.$watch 'projects.length' ->
+    $scope.featured = [p for p in $scope.projects when p.thumbnail]
--- javascript/app.js
+++ javascript/app.js
@@ -12,4 +12,23 @@ angular.module("g0v.tw", ['firebase']).factory({
   BlogCtrl: ['$scope', 'angularFireCollection', 'fireRoot'].concat(function($scope, angularFireCollection, fireRoot){
     return $scope.articles = angularFireCollection(fireRoot.child("feed/blog/articles").limit(10));
   })
+}).controller({
+  FeaturedCtrl: ['$scope', 'angularFireCollection'].concat(function($scope, angularFireCollection){
+    var g0vhub;
+    g0vhub = new Firebase("https://g0vhub.firebaseio.com/projects");
+    $scope.projects = angularFireCollection(g0vhub);
+    return $scope.$watch('projects.length', function(){
+      var p;
+      return $scope.featured = (function(){
+        var i$, ref$, len$, results$ = [];
+        for (i$ = 0, len$ = (ref$ = $scope.projects).length; i$ < len$; ++i$) {
+          p = ref$[i$];
+          if (p.thumbnail) {
+            results$.push(p);
+          }
+        }
+        return results$;
+      }());
+    });
+  })
 });
