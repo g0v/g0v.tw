47c1fe22d77da87c86fe87cde69ce1a6c333224b
Victor Hsieh <victor@csie.org>
2013-08-19 21:29:08 -0700

    remove grunt & jekyll

--- server.js
+++
@@ -1,6 +0,0 @@
-var port = 8080;
-var connect = require('connect');
-connect.createServer(
-    connect.static(__dirname)
-).listen(port);
-console.log('local server is running on port %d...',port);
